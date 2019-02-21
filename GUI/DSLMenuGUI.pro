f82d059d774afd6540bcffbb2fde1bbfe3f5992e
TBrown42 <timbrownx@gmail.com>
2019-02-21 08:59:48 -0500

    Add files via upload
    Code for basic digital clock display.

---
+++ GUI/DSLMenuGUI.pro
@@ -0,0 +1,13 @@
+TEMPLATE = app
+TARGET = DSLMenuGUI
+
+QT = core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+SOURCES += \
+    main.cpp \
+    digitalclock.cpp
+
+HEADERS += \
+    digitalclock.h
---
+++ GUI/digitalclock.cpp
@@ -0,0 +1,24 @@
+#include <QWidget>
+
+#include "digitalclock.h"
+
+DigitalClock::DigitalClock(QWidget *parent): QLCDNumber (parent){
+    setSegmentStyle(Filled);
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &DigitalClock::showTime);
+    timer->start(1000);
+
+    showTime();
+
+    setWindowTitle("Digital Clock");
+    resize(150,60);
+}
+
+void DigitalClock::showTime(){
+    QTime time = QTime::currentTime();
+    QString text = time.toString("hh:mm");
+    if((time.second() % 2) == 0)
+        text[2] = ' ';
+    display(text);
+}
